f89c818b97cdbb46402daccd1592ad0e34af08c3
namchn <45189669+namchn@users.noreply.github.com>
2022-09-27 00:04:23 +0900

    Add files via upload

--- server2.js
+++ server2.js
@@ -1,7 +1,43 @@
 const express = require("express");
+const router  = express.Router();
 const app = express();
 const fs = require("fs");
 
+
+
+/* */
+//////////////////////////////////////////////////////////////
+const mysql =require("mysql");
+let answer ="";
+let connection  =mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'username',
+	password:'12345678',
+	database: 'test'
+	
+	//user: 'root',
+	//password:'12345678',
+	//database: 'test'
+});
+
+connection.connect();
+connection.query('SELECT 1 + 1 AS solution', 
+function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+
+connection.query('SELECT writer_nm AS solution from jwarticle ', 
+function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+  answer = results[0].solution;
+});
+
+connection.end();
+//////////////////////////////////////////////////////////////
 
 
 //
@@ -10,7 +46,7 @@ let message2 = `
     <h1>Hello World</h1>
     <p>This is main page</p>
     `;
-let message3 = `<div>404</div><h1>Sorry, page not found :(</h1>`;
+let message3 = `<div>404</div><h1>Sorry, page not found :(</h1>` 
 //
 
 
@@ -20,7 +56,7 @@ let message3 = `<div>404</div><h1>Sorry, page not found :(</h1>`;
        console.log(files);
 	   
 	   files.forEach((file) => {
-        list += `<li>${file}</li>`;
+		list += `<li>${file}</li>`;
 		console.log(list);
       });
   });
@@ -47,15 +83,29 @@ app.get("/main", (request, response) => {
     const test = fs.readFileSync("./data/test.txt", "utf8");
     response.send(test);
   
-  
-  
 	//response.send(message2);
   
 });
 
+
+
+/* localhost:3000/test 접속시 나올 메시지 */
+app.get("/test", (request, response) => {  
+  
+    //alert('!!!!');
+	response.redirect('/');
+  
+});
+
+
+
+
 /* localhost:3000/ 혹은 localhost:3000/main 외의
 get하지 않은 페이지 접속시 나올 메시지. */
 app.use((request, response) => {
+	
+  message3 = message3 + '<div>db 로 나온값</div>'+ answer;
+	
   response.send(message3);
 });
 
